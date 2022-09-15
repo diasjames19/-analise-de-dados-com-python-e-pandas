6b1f9fa6ede074602f46d74d9626642c565ff851
jamesalvesdias37 <James_Alves_Dias@hotmail.com>
2022-09-15 14:39:36 -0300

    Criando checkout de Desenvolvimento

--- codigo/analise.py
+++ codigo/analise.py
@@ -3,8 +3,6 @@
 import pandas as pd
 
 
-print("Usando Pandas")
-
 dgf = pd.read_csv('Gapminder.csv',error_bad_lines=False, sep=";")
 #Alterando o cabeçalho da coluna!
 traducao_nome_tabela = dgf.rename(columns={'country':"Pais",'continent':"Continente",'year':"Ano",'lifeExp':"Exp de Vida",'pop':"Populacao Total",'gdpPercap':"Pib"})
